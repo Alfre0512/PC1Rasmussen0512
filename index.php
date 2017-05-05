1aef62a248c8c2754f35bf5d19e6e35d72e04a9d
Alfredo Gabriel Rasmussen Olivera <u201212616@upc.edu.pe>
2017-05-05 20:53:07 +0000

    V2

--- index.php
+++ index.php
@@ -5,7 +5,19 @@
  	<img src="images/top.jpg" width="1000" height="250"/>
  </div>
  <div name="middle" align="center">
+ 
  <table>
+ 
+	<tr>
+		<th style="font-size:24px;" align="left">For data scientists</th>
+		<th style="font-size:24px;" align="left">For IT professionals</th>
+	</tr>
+ 
+ <tr>
+ 
+<!-- ------------------------------------------------------ -->
+<td> 
+<table>
 	  	<?php
 		$server="us-cdbr-iron-east-03.cleardb.net";
 		$user="b74ba3320e82ec";
@@ -19,22 +31,17 @@
 		$result=$conn->query($sql);
 		if($result->num_rows > 0){
 			?>
-			<tr>
-				<th>Codigo</th>
-				<th>Nombre</th>
-				<th>Descripcion</th>
-				<th>Imagen</th>
-			</tr>
 			<?php
 			while($row=$result->fetch_assoc()){
+				if(($row['codigo_servicio']=='0001') || ($row['codigo_servicio']=='0002') || ($row['codigo_servicio']=='0003')){
 				?>
-				<tr>
-					<td><?php echo $row['codigo_servicio']?></td>
-					<td><?php echo $row['nombre_servicio']?></td>
-					<td><?php echo $row['descripcion_servicio']?></td>
-					<td><img src="<?php echo $row['imagen_servicio']?>"/></td>
+				<tr width="310" height="110">
+					<td width="100"><img src="<?php echo $row['imagen_servicio']?>"/></td>
+					<td width="400"><?php echo "<b>".$row['nombre_servicio']."</b>"."<br><br>".$row['descripcion_servicio']."<br>"?></td>
 				</tr>
+		
 				<?php
+				}
 			}
 		}
 		else{
@@ -43,6 +50,46 @@
 		$conn->close();
 		?>
 	  </table>
+</td>	  
+<!-- ------------------------------------------------------ -->
+<td>
+<table>
+	  	<?php
+		$server="us-cdbr-iron-east-03.cleardb.net";
+		$user="b74ba3320e82ec";
+		$pass="2d194843";
+		$db="ad_bb6ddb2f34daf52";
+		$conn=new mysqli($server, $user, $pass, $db);
+		if($conn->connect_error){
+			die("Connection Failed: ".$conn->connect_error);
+		}
+		$sql="select * from servicios";
+		$result=$conn->query($sql);
+		if($result->num_rows > 0){
+			?>
+			<?php
+			while($row=$result->fetch_assoc()){
+				if(($row['codigo_servicio']=='0004') || ($row['codigo_servicio']=='0005') || ($row['codigo_servicio']=='0006')){
+				?>
+				<tr width="310" height="110">
+					<td width="100"><img src="<?php echo $row['imagen_servicio']?>"/></td>
+					<td width="400"><?php echo "<b>".$row['nombre_servicio']."</b>"."<br><br>".$row['descripcion_servicio']."<br>"?></td>
+				</tr>
+				<?php
+				}
+			}
+		}
+		else{
+			echo "0 result";
+		}
+		$conn->close();
+		?>
+	  </table>
+</td>
+<!-- ------------------------------------------------------ -->
+</tr>
+</table>
+
 	 </div>
  <div name="bottom" align="center">
  	<img src="images/bot.jpg" width="1000" height="300"/>
